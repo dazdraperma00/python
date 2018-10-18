c27dce0aa3ddd46e1388d43f51d4a7054c29c195
dazdraperma00 <43701161+dazdraperma00@users.noreply.github.com>
2018-10-18 03:19:59 +0300

    Add files via upload

---
+++ text_history.py
@@ -0,0 +1,107 @@
+class TextHistory:
+    
+    def __init__(self):
+        self._text = ""
+        self._version = 0
+        self._get_actions = []
+        
+    @property
+    def text(self):
+        return self._text
+
+    @property
+    def version(self):
+        return self._version
+
+    def action(self, action):
+        if action.pos > len(self.text) or action.pos < 0:
+            raise ValueError
+
+        if action.to_version <= action.from_version:
+            raise ValueError
+        
+        self._text = action.apply(self._text)
+        self._version = action.to_version
+        self._get_actions.append(action)
+
+        return self._version
+
+
+    def insert(self, text, pos = None):
+        if pos is None:
+            pos = len(self.text)
+        a = InsertAction(pos, text, self._version, self._version + 1)
+        return self.action(a)
+
+
+    def replace(self, text, pos = None):
+        if pos is None:
+            pos = len(self.text)
+        a = ReplaceAction(pos, text, self._version, self._version + 1)
+        return self.action(a)
+
+    def delete(self, pos, length):
+        a = DeleteAction(pos, length, self._version, self._version + 1)
+        return self.action(a)
+
+    def get_actions(self, from_version = None, to_version = None):
+        if from_version is None and to_version is None:
+            return self._get_actions
+        
+        if from_version is None:
+            from_version = 0
+
+        if to_version is None:
+            to_version = self.version
+
+        if to_version < from_version or to_version < 0 or from_version < 0:
+            raise ValueError
+
+        first = -1
+        last = -1
+        for idx, item in enumerate(self._get_actions):
+            if item.from_version == from_version:
+                first = idx
+            if item.to_version == to_version:
+                last = idx 
+        if (first != -1 or last != -1) and from_version == to_version:
+            return []
+        if first == -1 or last == -1:
+            raise ValueError
+        else:
+            return self._get_actions[first : last + 1]
+        
+    
+class Action:
+    def __init__(self, pos, text, from_version, to_version):
+        self.pos = pos
+        self.text = text
+        self.from_version = from_version
+        self.to_version = to_version
+
+
+class InsertAction(Action):
+    
+    def apply(self, text):
+        return "{}{}{}".format(text[:self.pos], self.text, text[self.pos:])
+
+
+class ReplaceAction(Action):
+
+    def apply(self, text):
+        return "{}{}{}".format(text[:self.pos], self.text, text[self.pos + len(self.text):])
+
+        
+
+class DeleteAction(Action):
+    def __init__(self, pos, length, from_version, to_version):
+        self.pos = pos
+        self.length = length
+        self.from_version = from_version
+        self.to_version = to_version
+
+    def apply(self, text):
+        if self.pos + self.length > len(text):
+            raise ValueError
+        return "{}{}".format(text[:self.pos], text[self.pos + self.length:])
+        
